5422384bdcf7dd4530b8921517ed008075e269b4
Firefly Experience Ltd <alpha@firef.ly>
2015-05-19 23:13:31 +0100

    rebuild project in hopes of simpler integration

---
+++ EmitterKit/EmitterKit.h
@@ -0,0 +1,19 @@
+//
+//  EmitterKit.h
+//  EmitterKit
+//
+//  Created by Firefly Experience Ltd on 19/05/2015.
+//  Copyright (c) 2015 Firefly Experience Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EmitterKit.
+FOUNDATION_EXPORT double EmitterKitVersionNumber;
+
+//! Project version string for EmitterKit.
+FOUNDATION_EXPORT const unsigned char EmitterKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EmitterKit/PublicHeader.h>
+
+
